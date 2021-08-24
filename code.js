b6cbac94ac30a13eb1a92c96c92cbef3ca20bf77
juanfelipegarcia <jfgarcia064@misena.edu.co>
2021-08-24 18:44:45 -0500

    Proceso de crear, editar y eliminar listos

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 //Definicion de variables
-const url = 'http://localhost:3000/api/productos';
+const url = 'http://localhost:3000/api/productos/';
 
 // const url = "/dic.txt";
 const contenedor = document.querySelector('tbody');
@@ -34,8 +34,7 @@ const mostrar = (productos) =>{
                         <td>${producto.familia}</td> 
                         <td class="text-center">
                             <a class="btnEditar btn btn-primary">Editar</a>
-                            <a class="btnBorar btn btn-danger">Borar</a>
-                          
+                            <a class="btnBorrar btn btn-danger">Borar</a>
                         </td> 
                     </tr>
                     `
@@ -50,3 +49,95 @@ fetch(url)
     .then(response => response.json())
     .then(data => mostrar(data))
     .catch(error => console.log(error))
+
+const on = (element, event, selector, handler) => {	
+    // console.log(element);
+    // console.log(event);
+    // console.log(selector);
+    // console.log(handler);
+    element.addEventListener(event, e => {
+        if (e.target.closest(selector)) {
+            handler(e)
+        }
+    })
+}
+ 
+//Procedimiento para Borrar registro
+on(document, 'click', '.btnBorrar', e => {
+    const fila = e.target.parentNode.parentNode;
+    const id = fila.firstElementChild.innerHTML;
+    console.log(id);
+    alertify.confirm("This is a confirm dialog.",
+    function(){
+        fetch(url+id, {
+            method:'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => location.reload())
+        // alertify.success('Ok');
+    },
+    function(){
+        alertify.error('Cancel');
+    });
+})
+
+//Procedimiento para editar registro
+let idForm = 0;
+on(document, 'click', '.btnEditar', e => {
+    const fila = e.target.parentNode.parentNode;
+    idForm = fila.children[0].innerHTML;
+    const descripcionForm = fila.children[1].innerHTML;
+    const precioForm = fila.children[2].innerHTML;
+    const stockForm = fila.children[3].innerHTML;
+    const familiaForm = fila.children[4].innerHTML;
+
+    descripcion.value = descripcionForm;
+    precio.value = precioForm;	
+    stock.value = stockForm;
+    familia.value = familiaForm;
+    opcion = 'editar';
+    modalProducto.show();
+})
+
+//Procedimiento para crear y guardar
+formProducto.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if (opcion=='crear') {
+        // console.log('Opcion Crear');
+        fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                descripcion:descripcion.value,
+                precio:precio.value,
+                stock:stock.value,
+                familia:familia.value
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const nuevoProducto = [];
+            nuevoProducto.push(data);
+            mostrar(nuevoProducto);
+        })
+    }
+    if (opcion=='editar') {
+        fetch(url+idForm,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                descripcion:descripcion.value,
+                precio:precio.value,
+                stock:stock.value,
+                familia:familia.value
+            })
+        })
+        .then(response => response.json())
+        .then(response => location.reload())
+    }
+    modalProducto.hide();
+})
